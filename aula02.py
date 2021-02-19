d04cb5d6d7342bdb1d9aadf3ddca47f4daedc06d
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2021-02-18 23:35:40 -0300

    Commit com as primeiras aulas de Python.

---
+++ aula02.py
@@ -0,0 +1,13 @@
+#Comentário de uma unica linha => #Sintaxe Básica P2 e Comentários.
+num = 1
+num = "Bruno"
+num = 1.5
+""" Comentário de multiplas linhas são as 3 aspas duplas"""
+canal = "CFB Cursos"
+curso = "Curso de Python"
+print(canal + " - " + curso)
+
+if 10 > 2:
+    print("Maior")
+    print("aula 2")
+print("FIM")
