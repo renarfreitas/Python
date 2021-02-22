ce6e32d107fc91efea5ada27cc437cdcecf62e92
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2021-02-21 21:36:11 -0300

    Commit da aula de Matemática Básica
    Commit de estudos sobre Matemática Básica usando python.

---
+++ Matematica_ba.py
@@ -0,0 +1,79 @@
+# Matemática Básica. Aqui teremos um curso completo que irá revisar toda a 
+#Matemática Básica que você precisa estudar para o ENEM, os vestibulares, 
+#concursos públicos e a escola e para toda vida.
+
+                              #Soma e Subtração
+
+print("\n*****************************\n")
+print("Soma de números inteiros:")
+
+num01 = 3875
+num02 = 763
+
+soma = num01 + num01
+
+print("",num01)
+print("+",num02)
+print("-----")
+print("",soma)
+print("\n*****************************\n")
+#Soma de números decimais
+#Operações de adição entre números decimais :
+#Obs* : "Zeros", no final da escrita decimal não alteram o valo do número representado.
+        #Compare números decimais começando pela parte inteira. Depois compare os décimos, centésimos, milésimos, etc.
+#ex: 1,2 = 1,20
+print("\n*****************************\n")
+print("Soma de números decimais:")
+num03 = 576.28
+num04 = 94.27
+
+soma_d = num03 + num04
+print("",num03)
+print("+",num04)
+print("---------")
+print("",soma_d)
+print("\n*****************************\n")
+
+#Propriedades da Adição:
+  #1. Elemnto neutro:
+    # a + 0 = a
+  
+  #2. Comutativa:
+    # a + b = b + a
+  
+  #3. Associativa:
+    # (a + b) + c = a + (b + c)
+
+#Subtração
+
+#Operação de subtração com números inteiros
+#1. Oposto de um número, sempre será o sinal contrário
+      #ex: O oposto do número: 5 => -5,
+    #O Oposto do número é nós trocarmos o sinal dele:
+      #ex: -(-3) => 3
+#2. a -(b + c) = a - n - c
+    # Ou seja, o sinal de negativo na frente do parenteses,
+    #provoca a mudança de sinal (+ou-), de todos os números dentro do parenteses.
+
+print("\n*****************************\n")
+print("Subtração de números inteiros:")
+num05 = 1938
+num06 = 375
+
+soma_sub = num05 - num06
+print("",num05)
+print("-",num06)
+print("---------")
+print("",soma_sub)
+print("\n*****************************\n")
+print("\n*****************************\n")
+print("Subtração de números decimais:")
+num07 = 475.08
+num08 = 86.50
+
+soma_subd = num07 - num08
+print("",num07)
+print("-",num08)
+print("---------")
+print("",soma_subd)
+print("\n*****************************\n")
