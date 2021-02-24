48643211a9eec5e51791cd48260a7ccaf0e4fdbd
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2021-02-23 22:50:01 -0300

    Commit da aula 04 de Python

---
+++ aula04.py
@@ -0,0 +1,26 @@
+#Tipos básicos.
+
+x = 1 #int
+x = "CFB Cursos" #string
+X = 15.6 #float
+x = True #bool
+x = ["carro", "avião", "navio"] #Uma lista / List / Array
+x = ("carro", "avião", "navio") #Tupla
+x = range(0,100,1) #List
+# x = {
+#    "Canal":"CFB Cursos"
+#    "Curso":"Python"
+#    "Nome":"Renar" 
+#}
+
+x = {5,7,4,5,7,4,8}
+x = frozenset({5,7,4,5,7,4,8})
+
+#números complexos
+#n1 = 5; n2 = 2; x = complex(1j)
+
+#print(x.real)
+#print(x.imag)
+
+print("Valor: "+str(x))
+print("Tipo: " +str(type(x)))
