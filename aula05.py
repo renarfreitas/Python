4b2b102e4a66039697a864bbccf64d23d5e2bb77
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2021-02-26 21:45:31 -0300

    Commit da aula 05 Curso de Python
    Tipos numéricos, random e operações de casting - Curso de Python #05
    Com o professor Bruno do canal: CFB - Cursos.

---
+++ aula05.py
@@ -0,0 +1,20 @@
+import random
+num_i = 10
+num_f = 5.2
+num_c = 1j
+
+num_r = [ #List / Array
+    random.randrange(0,59),
+    random.randrange(0,59),
+    random.randrange(0,59),
+    random.randrange(0,59),
+    random.randrange(0,59),
+    random.randrange(0,59)
+    ]
+
+print("Valor 1: " + str(num_r[0]))
+print("Valor 2: " + str(num_r[1]))
+print("Valor 3: " + str(num_r[2]))
+print("Valor 4: " + str(num_r[3]))
+print("Valor 5: " + str(num_r[4]))
+print("Valor 6: " + str(num_r[5]))
