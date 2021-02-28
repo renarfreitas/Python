16cf0a8f45801199e97cdb79f6efb6e81607731b
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2021-02-28 00:21:44 -0300

    Commit da aula - Tipo Boolean
    Commit da aula  #08
    Curso: Python;
    Professor: Bruno;
    Canal: CFB Cursos.

---
+++ aula08.py
@@ -0,0 +1,11 @@
+#Tipo Boolean - Curso de Python #08
+#Tipos booleanos recebe valores, verdadeiros ou falsos
+#aula = 10>15
+aula = ""
+
+#if aula:
+#    print("Possui texto")
+#else:
+#    print("Vazio")
+
+print(bool(aula))
