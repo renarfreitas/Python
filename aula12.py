094585bf08472ce61a8ed651438300a36a2e68eb
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2021-03-05 20:27:22 -0300

    Commit continuação da Aula de Python
    Loop FOR - Extruturas de repetição - Curso de Python #12
    Curso: Python;
    Professor: Bruno;
    Canal: CFB Cursos.

---
+++ aula12.py
@@ -0,0 +1,9 @@
+#Loop FOR - Extruturas de repetição - Curso de Python #12
+carros = ["HRV", "Golf", "Argo", "Focus", "Fit", "Fusion", "Polo"]
+
+for x in carros:
+    if(x == "Fit"):
+        break
+    print(x)
+
+print("Fim do programa")
