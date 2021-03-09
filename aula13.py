fcd9607e950a6f904f2e9c647ab29d96b76ca6d9
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2021-03-08 23:28:39 -0300

    Commit da aula13 do Curso de Python
    Função Input - Curso de Python #13
    Professor: Bruno
    Canal: CFB Cursos.

---
+++ aula13.py
@@ -0,0 +1,11 @@
+import os
+
+os.system('clear')
+#Função input = Curso de Pytho #13
+#nome = input("Digite o seu nome: ")
+#print ("O nome digitado foi:", nome)
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+soma = num1 + num2
+print(soma)
