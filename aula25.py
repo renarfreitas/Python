b06eef7cc813cf71ec7c99ccfa2b1f72ee6d0fe6
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2021-03-22 22:24:21 -0300

    Aula 25 do Curso de Python
    POO - Herança / P3 - Curso de Python #25
    Professsor: Bruno
    Canal: CFB - Cursos

--- aula25.py
+++ aula25.py
@@ -1,28 +1,50 @@
-#Try Except / tratamento de erros - Curso de Python #26
+#25POO - Herança / P3 - Curso de Python #25
+class NPC: #Base, Pai, Super
+    def __init__(self, nome, time, forca, municao):
+        self.nome = nome
+        self.time = time
+        self.forca = forca
+        self.municao = municao
+        self.vivo = True
+        self.energia = 100
+    def info(self):
+        print("Nome....: " + self.nome)
+        print("Time....: " + str(self.time))
+        print("Força...: " + str(self.forca))
+        print("Munição.: " + str(self.municao))
+        print("Vivo....: " + ("sim" if self.vivo else "nao"))
+        print("Energia.: " + str(self.energia))
+        print("---------------------------")
 
-try:
-    print(x)
-except NameError:
-    print("X não está definido!")
-except:
-    print("Erro desconhecido!")
+class Soldado(NPC):
+    def __init__(self, nome, time):
+        self.forca = 200
+        self.municao = 200
+        super().__init__(nome, time, self.forca, self.municao)
 
-num = -10
-if num < 1:
-    raise Exception("Valor não permitido!")
+class Guarda(NPC):
+    def __init__(self, nome, time):
+        self.forca = 100
+        self.municao = 20
+        super().__init__(nome, time, self.forca, self.municao)
 
-try:
-    print(x)
-except:
-    print("Erro no programa")
-else:
-    print("Tudo certo")
-finally:
-    print("Fim do tratamento")
+class Elite(NPC):
+    def __init__(self, nome, time):
+        self.forca = 400
+        self.municao = 500
+        super().__init__(nome, time, self.forca, self.municao)
+        def inf(self):
+            super().inf()
+s1 = Guarda("Olho Vivo", 1)
+s2 = Soldado("Bala na Agulha", 1)
+s3 = Elite("Ninja", 1)
+s4 = Guarda("Super Atento", 1)
+s5 = Soldado("Tiro Certo", 1)
+s6 = Elite("Samurai", 1)
 
-num = "Renar"
-
-if not type(num) is int:
-    raise Exception("Somente números permitidos")
-else:
-    print(num)
+s1.info()
+s2.info()
+s3.info()
+s4.info()
+s5.info()
+s6.info()
