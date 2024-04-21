53db29ccbb04e3a075b1d6bcba87416681aed8c2
Renar Freitas <renarfreitas@gmail.com>
2024-04-20 22:51:57 -0300

    Part2

--- Python2024/app.py
+++ Python2024/app.py
@@ -1,8 +1,9 @@
 import streamlit as st # interface web
 import pandas as pd # manipulação de dados
 
+st.set_page_config(layout="wide")
 
-df_reviews = pd.read_csv('datasets/')
-df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
+df_reviews = pd.read_csv("datasets/customer_reviews.csv")
+df_top100_books = pd.read_csv("datasets/customer_reviews.csv")
 
-df_reviewss
+df_reviews
